3d4ae25fc0fdd59b2bcc11f0f89b069873823ca9
ronniej <ronniej@naver.com>
2026-01-26 16:08:24 +0900

    게임 플레이 이벤트 구성
    - Level/Actor 클래스 추가
    - BeginPlay/Tick/Draw 이벤트를 Engine->Level->Actor 까지 전달

--- Engine/Engine/Engine.cpp
+++ Engine/Engine/Engine.cpp
@@ -1,4 +1,6 @@
 #include "Engine.h"
+#include "Level/Level.h"
+
 #include <iostream>
 #include <Windows.h>
 
@@ -56,6 +58,7 @@ namespace Wanted
 				ProcessInput();
 
 				// 프레임 처리.
+				BeginPlay();
 				Tick(deltaTime);
 				Draw();
 
@@ -65,7 +68,7 @@ namespace Wanted
 				// 현재 입력 값을 이전 입력 값으로 저장.
 				for (int ix = 0; ix < 255; ++ix)
 				{
-					keyStates[ix].wasKeyDown 
+					keyStates[ix].wasKeyDown
 						= keyStates[ix].isKeyDown;
 				}
 			}
@@ -97,6 +100,21 @@ namespace Wanted
 		return keyStates[keyCode].isKeyDown;
 	}
 
+	void Engine::SetNewLevel(Level* newLevel)
+	{
+		// 기존 레벨 있는지 확인.
+		// 있으면 기존 레벨 제거.
+		// Todo: 임시 코드. 레벨 전환할 때는 바로 제거하면 안됨.
+		if (mainLevel)
+		{
+			delete mainLevel;
+			mainLevel = nullptr;
+		}
+
+		// 레벨 설정.
+		mainLevel = newLevel;
+	}
+
 	void Engine::ProcessInput()
 	{
 		// 키 마다의 입력 읽기.
@@ -108,20 +126,54 @@ namespace Wanted
 		}
 	}
 
+	void Engine::BeginPlay()
+	{
+		// 레벨이 있으면 이벤트 전달.
+		if (!mainLevel)
+		{
+			// Silent is violent.
+			// 침묵은 폭력이다.
+			// -> 로그 메시지 안남기면 나빠.
+			std::cout << "mainLevel is empty.\n";
+			return;
+		}
+
+		mainLevel->BeginPlay();
+	}
+
 	void Engine::Tick(float deltaTime)
 	{
-		std::cout
-			<< "DeltaTime: " << deltaTime
-			<< ", FPS: " << (1.0f / deltaTime) << "\n";
+		//std::cout
+		//	<< "DeltaTime: " << deltaTime
+		//	<< ", FPS: " << (1.0f / deltaTime) << "\n";
 
-		// ESC키 눌리면 종료.
-		if (GetKeyDown(VK_ESCAPE))
+		//// ESC키 눌리면 종료.
+		//if (GetKeyDown(VK_ESCAPE))
+		//{
+		//	QuitEngine();
+		//}
+
+		// 레벨에 이벤트 흘리기.
+		// 예외처리.
+		if (!mainLevel)
 		{
-			QuitEngine();
+			std::cout << "Error: Engine::Tick(). mainLevel is empty.\n";
+			return;
 		}
+
+		mainLevel->Tick(deltaTime);
 	}
 
 	void Engine::Draw()
 	{
+		// 레벨에 이벤트 흘리기.
+		// 예외처리.
+		if (!mainLevel)
+		{
+			std::cout << "Error: Engine::Draw(). mainLevel is empty.\n";
+			return;
+		}
+
+		mainLevel->Draw();
 	}
 }

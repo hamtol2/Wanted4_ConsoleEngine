22e379fe74d0ce62c9e83b7ee82f7d263d81fa66
ronniej <ronniej@naver.com>
2026-01-30 16:34:48 +0900

    엔진 리팩토링 및 기능 추가 및 데모
    Actor
    - Draw/SetPosition 로직 정리
    - Level* owner 추가 (오너십)
    
    Engine
    - Shutdown 함수 추가 (엔진 종료 시 호출)
    
    런타임 액터 추가 데모

--- Engine/Actor/Actor.cpp
+++ Engine/Actor/Actor.cpp
@@ -1,18 +1,22 @@
 #include "Actor.h"
 #include "Util/Util.h"
+#include "Core/Renderer.h"
+
 #include <iostream>
 #include <Windows.h>
 
 namespace Wanted
 {
-	Actor::Actor(const char image, const Vector2& position)
-		: image(image), position(position)
+	Actor::Actor(
+		const char image, 
+		const Vector2& position,
+		Color color)
+		: image(image), position(position), color(color)
 	{
 	}
 
 	Actor::~Actor()
 	{
-
 	}
 
 	void Actor::BeginPlay()
@@ -27,20 +31,14 @@ namespace Wanted
 
 	void Actor::Draw()
 	{
-		// 액터의 현재 좌표로 콘솔 좌표 위치 이동.
-		Util::SetConsolePosition(position);
-
-		// 이동한 좌표에서 글자 그리기.
-		std::cout << image;
+		// 렌더러에 그리기 요청.
+		Renderer::Draw(position, color, image);
 	}
 
 	void Actor::SetPosition(const Vector2& newPosition)
 	{
-		// 액터의 현재 좌표로 콘솔 좌표 위치 이동.
-		Util::SetConsolePosition(position);
-
-		// 해당 위치의 글자 값 지우기 (빈칸 그리기).
-		std::cout << ' ';
+		// 렌더러에 빈칸 그리기 요청.
+		Renderer::Draw(position, ' ');
 
 		// 새로운 위치 설정.
 		position = newPosition;
